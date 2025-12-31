151ee5967ba27e1992aa5e7b4973cffb6bcb3bd7
zVaiku <joshreed1ee@gmail.com>
2025-12-31 18:42:04 -0500

    major major changes

--- include/Handler.hpp
+++ include/Handler.hpp
@@ -6,6 +6,7 @@
 #include <type_traits>
 #include <format>
 #include <string>
+#include <atomic>
 
 namespace phosphor::log {
 
@@ -17,20 +18,19 @@ enum class LOG_LEVEL {
     NONE
 };
 
-inline LOG_LEVEL logLevel = LOG_LEVEL::INFO;
+inline std::atomic<LOG_LEVEL> logLevel{LOG_LEVEL::INFO};
 
-inline const char* toString(LOG_LEVEL level) {
+inline std::string toString(LOG_LEVEL level) {
     switch (level) {
         case LOG_LEVEL::INFO: return "INFO";
         case LOG_LEVEL::WARN: return "WARN";
         case LOG_LEVEL::ERROR: return "ERROR";
         case LOG_LEVEL::DEBUG: return "DEBUG";
-        default: return "UNKNOWN"; 
+        default: return "NONE"; 
     }
 }
 
-inline const std::string& getTime() {
-    static thread_local char buf[16];
+[[nodiscard]] inline std::string getTime() {
     auto now = std::chrono::system_clock::now();
     std::time_t time = std::chrono::system_clock::to_time_t(now);
     std::tm tm = {};
@@ -38,6 +38,7 @@ inline const std::string& getTime() {
 #if defined(_WIN32)
     localtime_s(&tm, &time);
 #elif defined(__linux__)
+    // no need for __APPLE__ because xnu is linux compatible
     localtime_r(&time, &tm);
 #else
     #error i dont know how to get the time on this platform!
@@ -58,15 +59,12 @@ public:
     static void logCstr(const char* str);
 
     inline void formatImpl(const char* fmt) {
-        while (*fmt) {
-            std::fputc(*fmt, stdout);
-            ++fmt;
-        }   
+        while (*fmt)
+            std::fputc(*fmt++, stderr);
     }
 
     template<typename T, typename... Args>
     void formatImpl(const char* fmt, T val, Args... args);
-    
     template<typename... Args>
     void logFmt(LOG_LEVEL level, const char* fmt, Args... args);
 };
@@ -76,7 +74,7 @@ namespace detail {
 inline void dispatchLog(char spec, int val) {
     switch (spec) {
         case 'd': core::logInt(val); break;
-        default: std::fputc('?', stdout); break;
+        default: std::fputc('?', stderr); break;
     }
 }
 
@@ -86,7 +84,7 @@ inline void dispatchLog(char spec, const char* val) {
         case 'p':
             core::logHex(reinterpret_cast<uintptr_t>(val));
             break;
-        default: std::fputc('?', stdout); break;
+        default: std::fputc('?', stderr); break;
     }
 }
 
@@ -97,8 +95,40 @@ void dispatchLog(char spec, T* val) {
         case 'x':
             core::logHex(reinterpret_cast<uintptr_t>(val));
             break;
-        default: std::fputc('?', stdout); break;
+        default: std::fputc('?', stderr); break;
     }
+}
+
+constexpr std::size_t ctGetSpecs(const char* spec) {
+    std::size_t n = 0;
+    for (std::size_t i = 0; spec[i]; ++i) {
+        if (
+            spec[i] == '{' &&
+            spec[i+1] == ':' &&
+            spec[i+2] &&
+            spec[i+3] == '}'
+        ) {
+            ++n;
+            i += 3;
+        }
+    }
+    return n;
+}
+
+inline std::size_t rtGetSpecs(const char* spec) {
+    std::size_t n = 0;
+    for (; *spec; ++spec) {
+        if (
+            spec[0] == '{' &&
+            spec[1] == ':' &&
+            spec[2] &&
+            spec[3] == '}'
+        ) {
+            ++n;
+            spec += 3;
+        }
+    }
+    return n;
 }
 
 } // namespace detail   
--- include/Template.hpp
+++ include/Template.hpp
@@ -6,25 +6,59 @@ namespace phosphor::log {
 template<typename T, typename... Args>
 void core::formatImpl(const char* fmt, T val, Args... args) {
     while (*fmt) {
-        if (fmt[0] == '{' && fmt[1] == ':' && fmt[3] == '}') {
-            char spec = fmt[2];
-            detail::dispatchLog(spec, val);
-
+        if (
+            fmt[0] == '{' &&
+            fmt[1] == ':' &&
+            fmt[2] &&
+            fmt[3] == '}'
+        ) {
+            detail::dispatchLog(fmt[2], val);
             fmt += 4;
-            formatImpl(fmt, args...);
+            
+            if constexpr (sizeof...(Args) > 0)
+                formatImpl(fmt, args...);
+            else {
+                std::fputs("\n[phosphor]: missing format arguments\n", stderr);
+                std::abort();
+            }
             return;
         }
-        std::fputc(*fmt++, stdout);
+        std::fputc(*fmt++, stderr);
     }
+
+    std::fputs("\n[phosphor]: too many arguments\n", stderr);
+    std::abort();
 }
 
 template<typename... Args>
 void core::logFmt(LOG_LEVEL level, const char* fmt, Args... args) {
-    if (level > logLevel) return;
+#if defined(__GNUC__) || defined(__clang__)
+    if constexpr (__builtin_constant_p(fmt))
+        static_assert(
+            detail::ctGetSpecs(fmt) == sizeof...(Args),
+            "[phosphor]: format/argument count mismatch"
+        );
+    else
+#endif
+    {
+        if (detail::rtGetSpecs(fmt) != sizeof...(Args)) {
+            std::fputs("[phosphor]: format/argument count mismatch\n", stderr);
+            std::abort();
+        }
+    }
 
-    printf("[%s, %s]: ", getTime(), toString(level));
+    if (level < logLevel.load(std::memory_order_relaxed))
+        return;
+
+    std::fprintf(
+        stderr,
+        "[%s, %s]: ",
+        getTime().c_str(),
+        toString(level).c_str()
+    );
+
     formatImpl(fmt, args...);
-    std::fputc('\n', stdout);
+    std::fputc('\n', stderr);
 }
 
 } // namespace phosphor::log
